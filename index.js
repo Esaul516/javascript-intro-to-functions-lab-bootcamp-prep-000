6455b6bf88041901d68638649acf4ef87ee46020
Ethan Saul <saul.ethan516@gmail.com>
2017-08-25 14:17:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ var sayHiToGrandma = 'hello'
     console.log("I can't hear you!")
   } else if (sayHiToGrandma === "HELLO") {
     console.log("YES INDEED!")
-  } else (sayHiToGrandma === "I love you Grandma"){
+  } else (sayHiToGrandma === "I love you Grandma") {
     console.log("I love you, too!")
 }
